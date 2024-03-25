a06a4360f10fc01b66342239f83c77de473d96e9
HojuShin <shin03188@naver.com>
2024-03-25 17:48:43 +0900

    style: 노이즈 이펙트 활용

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,7 +1,13 @@
+import '../../styles/home.css';
+
 export default function Home() {
     return (
-        <div className="App">
+        <>
+         <div className="App">
             테스트
         </div>
+        <div className="noise"></div>
+        </>
+       
     );
 }
