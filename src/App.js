3fb04d8b9f39ed5a6748c2fefcac14bd1fe4cc9a
HojuShin <shin03188@naver.com>
2024-03-25 17:21:58 +0900

    feat: 리액트 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/home';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home/home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="App">
+            테스트
+        </div>
+    );
+}
