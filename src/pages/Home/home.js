b38b0326f139dba6fc0eb135799d718d32bd7ccb
HojuShin <shin03188@naver.com>
2024-04-01 15:17:36 +0900

    feat: 활성화된 네비게이션 항목에 테두리 스타일 추가

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react';
 import Background from '../../components/Background';
 import '../../styles/home.css'
 
 export default function Home() {
+
+    const [position, setPosition] = useState('home')
 
     const scrollCallBack = (targetId) => {
         document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
@@ -16,10 +19,26 @@ export default function Home() {
                     <div className='header-nav'>
                         <nav>
                             <ul>
-                                <li onClick={() => scrollCallBack('home')}><p>Home</p></li>
-                                <li onClick={() => scrollCallBack('aboutme')}><p>About Me</p></li>
-                                <li onClick={() => scrollCallBack('portfolio')}><p>Portfolio</p></li>
-                                <li onClick={() => scrollCallBack('contact')}><p>Contact</p></li>
+                                <li className={position === 'home' ? 'active' : ''}
+                                    onClick={() => {
+                                        scrollCallBack('home')
+                                        setPosition('home')
+                                    }}><p>Home</p></li>
+                                <li className={position === 'aboutme' ? 'active' : ''}
+                                    onClick={() => {
+                                        scrollCallBack('aboutme')
+                                        setPosition('aboutme')
+                                    }}><p>About Me</p></li>
+                                <li className={position === 'portfolio' ? 'active' : ''}
+                                    onClick={() => {
+                                        scrollCallBack('portfolio')
+                                        setPosition('portfolio')
+                                    }}><p>Portfolio</p></li>
+                                <li className={position === 'contact' ? 'active' : ''}
+                                    onClick={() => {
+                                        scrollCallBack('contact')
+                                        setPosition('contact')
+                                    }}><p>Contact</p></li>
                             </ul>
                         </nav>
                     </div>
