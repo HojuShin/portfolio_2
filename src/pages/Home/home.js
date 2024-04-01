0c32b39958e5ea39de18bdfd5e57ac604c774baf
HojuShin <shin03188@naver.com>
2024-04-01 14:45:47 +0900

    feat: 네비게이션 생성 및  버튼 클릭시 특정 영역으로 스크롤 이동

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,56 @@
-import '../../styles/home.css';
+import Background from '../../components/Background';
+import '../../styles/home.css'
 
 export default function Home() {
+
+    const scrollCallBack = (targetId) => {
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    }
+
     return (
         <>
-         <div className="App">
-            테스트
-        </div>
-        <div className="noise"></div>
+            <div className="App">
+                <Background />
+
+                <header id='header'>
+                    <div className='header-nav'>
+                        <nav>
+                            <ul>
+                                <li onClick={() => scrollCallBack('home')}><p>Home</p></li>
+                                <li onClick={() => scrollCallBack('aboutme')}><p>About Me</p></li>
+                                <li onClick={() => scrollCallBack('portfolio')}><p>Portfolio</p></li>
+                                <li onClick={() => scrollCallBack('contact')}><p>Contact</p></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className='header-mode'>
+                        <div className='lightmode'>
+                            <p>Light Mode</p>
+                        </div>
+                    </div>
+                </header>
+
+                <div className='container'>
+                    <div className='container-content'>
+
+                        <section id='home'>
+
+                        </section>
+
+                        <section id='aboutme' >
+
+                        </section>
+
+                        <section id='portfolio'>
+
+                        </section>
+                        <section id='contact'>
+
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <div className="noise"></div>
         </>
-       
     );
 }
