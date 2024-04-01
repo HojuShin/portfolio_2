1d1b94cb7ae450d4d3d0955c1e6470eb0e09f2b3
HojuShin <shin03188@naver.com>
2024-04-01 16:15:23 +0900

    feat: 스크롤 위치 추적으로 상태값 업데이트 기능

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -9,6 +9,32 @@ export default function Home() {
     const scrollCallBack = (targetId) => {
         document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
     }
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            const aboutMeSection = document.getElementById('aboutme');
+            const portfolioSection = document.getElementById('portfolio');
+            const contactSection = document.getElementById('contact');
+
+            const scrollY = window.scrollY || window.pageYOffset;
+
+            if (scrollY >= contactSection.offsetTop) {
+                setPosition('contact');
+            } else if (scrollY >= portfolioSection.offsetTop) {
+                setPosition('portfolio');
+
+            } else if (scrollY >= aboutMeSection.offsetTop) {
+                setPosition('aboutme');
+            } else {
+                setPosition('home');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); // useEffect는 한 번만 실행되도록 빈 배열을 전달합니다.
 
     return (
         <>
@@ -22,22 +48,22 @@ export default function Home() {
                                 <li className={position === 'home' ? 'active' : ''}
                                     onClick={() => {
                                         scrollCallBack('home')
-                                        setPosition('home')
+
                                     }}><p>Home</p></li>
                                 <li className={position === 'aboutme' ? 'active' : ''}
                                     onClick={() => {
                                         scrollCallBack('aboutme')
-                                        setPosition('aboutme')
+
                                     }}><p>About Me</p></li>
                                 <li className={position === 'portfolio' ? 'active' : ''}
                                     onClick={() => {
                                         scrollCallBack('portfolio')
-                                        setPosition('portfolio')
+
                                     }}><p>Portfolio</p></li>
                                 <li className={position === 'contact' ? 'active' : ''}
                                     onClick={() => {
                                         scrollCallBack('contact')
-                                        setPosition('contact')
+
                                     }}><p>Contact</p></li>
                             </ul>
                         </nav>
