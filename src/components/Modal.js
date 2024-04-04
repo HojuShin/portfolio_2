8b453633f0e13cd662797fd666c01da47533a503
HojuShin <shin03188@naver.com>
2024-04-05 04:48:41 +0900

    feat: 모달창 토글 및 URL id값 부여

---
+++ src/components/Modal.js
@@ -0,0 +1,7 @@
+export default function Modal() {
+    return (
+        <>
+            <div className="modal-section"></div>
+        </>
+    )
+} 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import watflix from '../assets/mockup.png'
 import apple from '../assets/mockup2.png'
 import gentle from '../assets/mockup3.png'
 import pro from '../assets/mockup4.png'
 import portfolio01 from '../assets/mockup5.png'
 import { ScrollAnimation } from "@lasbe/react-scroll-animation";
+import Modal from './Modal'
 
 export default function Portfolio() {
+
+    // 모달 상태값 
+    const [modal, setModal] = useState(false);
+
+    // 모달 열고 닫는 함수 정의
+    const toggle = () => {
+        // modal 상태값을 현재의 반대로 변경
+        setModal(!modal);
+        // 모달이 열린 경우 URL에 #modal 추가, 닫힌 경우 #modal 제거
+        if (!modal) {
+            window.history.pushState(null, null, '#modal');
+        } else {
+            window.history.pushState(null, null, window.location.pathname)
+        }
+    }
+
     return (
         <div className='portfolio-block'>
             <ScrollAnimation
@@ -13,29 +31,28 @@ export default function Portfolio() {
                 duration={1}
                 amount="xl"
                 delay={0.1}
-                repeat
+                repeat={false}
             >
                 <div className='section-text'>
                     <h2>Check my projects</h2>
                 </div>
             </ScrollAnimation>
-            {/* <video src={video} loop autoPlay muted className='video'></video> */}
             <div className='portfolio-projects'>
                 <ScrollAnimation
                     startingPoint="bottom"
                     duration={1}
                     amount="xl"
                     delay={0.2}
-                    repeat
+                    repeat={false}
                 >
-                    <div className='project'>
+                    <div className='project' onClick={toggle}>
                         <div className='project-mockup'>
                             <img src={pro} alt='watflix' />
                         </div>
                         <div className='project-btn'>
                             <div className='view-btn'>
                                 <p>Learn more</p>
-                                <span class="material-symbols-outlined">arrow_forward</span>
+                                <span className="material-symbols-outlined">arrow_forward</span>
                             </div>
                         </div>
                     </div>
@@ -46,16 +63,16 @@ export default function Portfolio() {
                     duration={1}
                     amount="xl"
                     delay={0.3}
-                    repeat
+                    repeat={false}
                 >
-                    <div className='project'>
+                    <div className='project' onClick={toggle}>
                         <div className='project-mockup'>
                             <img src={watflix} alt='watflix' />
                         </div>
                         <div className='project-btn'>
                             <div className='view-btn'>
                                 <p>Learn more</p>
-                                <span class="material-symbols-outlined">arrow_forward</span>
+                                <span className="material-symbols-outlined">arrow_forward</span>
                             </div>
                         </div>
                     </div>
@@ -66,16 +83,16 @@ export default function Portfolio() {
                     duration={1}
                     amount="xl"
                     delay={0.4}
-                    repeat
+                    repeat={false}
                 >
-                    <div className='project'>
+                    <div className='project' onClick={toggle}>
                         <div className='project-mockup'>
                             <img src={apple} alt='watflix' />
                         </div>
                         <div className='project-btn'>
                             <div className='view-btn'>
                                 <p>Learn more</p>
-                                <span class="material-symbols-outlined">arrow_forward</span>
+                                <span className="material-symbols-outlined">arrow_forward</span>
                             </div>
                         </div>
                     </div>
@@ -86,16 +103,16 @@ export default function Portfolio() {
                     duration={1}
                     amount="xl"
                     delay={0.5}
-                    repeat
+                    repeat={false}
                 >
-                    <div className='project'>
+                    <div className='project' onClick={toggle}>
                         <div className='project-mockup'>
                             <img src={gentle} alt='watflix' />
                         </div>
                         <div className='project-btn'>
                             <div className='view-btn'>
                                 <p>Learn more</p>
-                                <span class="material-symbols-outlined">arrow_forward</span>
+                                <span className="material-symbols-outlined">arrow_forward</span>
                             </div>
                         </div>
                     </div>
@@ -106,16 +123,16 @@ export default function Portfolio() {
                     duration={1}
                     amount="xl"
                     delay={0.6}
-                    repeat
+                    repeat={false}
                 >
-                    <div className='project'>
+                    <div className='project' onClick={toggle}>
                         <div className='project-mockup'>
                             <img src={portfolio01} alt='portfolio' />
                         </div>
                         <div className='project-btn'>
                             <div className='view-btn'>
                                 <p>Learn more</p>
-                                <span class="material-symbols-outlined">arrow_forward</span>
+                                <span className="material-symbols-outlined">arrow_forward</span>
                             </div>
                         </div>
                     </div>
@@ -126,21 +143,24 @@ export default function Portfolio() {
                     duration={1}
                     amount="xl"
                     delay={0.7}
-                    repeat
+                    repeat={false}
                 >
-                    <div className='project'>
+                    <div className='project' onClick={toggle}>
                         <div className='project-mockup'>
                             <img src={watflix} alt='watflix' />
                         </div>
                         <div className='project-btn'>
                             <div className='view-btn'>
                                 <p>Learn more</p>
-                                <span class="material-symbols-outlined">arrow_forward</span>
+                                <span className="material-symbols-outlined">arrow_forward</span>
                             </div>
                         </div>
                     </div>
                 </ScrollAnimation>
             </div>
+
+            {modal ? <Modal /> : null}
         </div>
+
     )
 }
